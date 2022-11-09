78667b18357dd90c17b951b843feacacadd83ea7
Nicolas Haase <nh@lutzhaase.com>
2022-11-09 10:19:43 +0100

    wip

---
+++ app/business/undohistory/delta/DeltaHistory.cs
@@ -0,0 +1,12 @@
+﻿using app.business.actionhistory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace app.business.undohistory.delta
+{
+    public class DeltaHistory : IUndoHistory<>
+    {
+    }
+}
